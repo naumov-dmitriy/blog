9344eaae2268693889239b633bd8fd3b694fbc5a
Dmitriy <investacc@yandex.ru>
2018-08-17 20:18:21 +0500

    Add edit and destroy posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [ :show, :edit, :update, :destroy ]
   def index
     @posts = Post.all
   end
   def show
-    @post = Post.find(params[:id])
   end
   def new
     @post = Post.new
@@ -16,7 +16,24 @@ class PostsController < ApplicationController
       render :new
     end
   end
+  def edit
+  end
+  def update
+    if @post.update_attributes(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private
+    def set_post
+      @post = Post.find(params[:id])
+    end
     def post_params
       params.require(:post).permit(:title, :summary, :body)
     end
