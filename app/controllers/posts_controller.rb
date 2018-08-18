cc0477dc43da9065dd68f21fdb5c0b9c75cc91d4
Dmitriy <investacc@yandex.ru>
2018-08-18 19:15:02 +0500

    Add flash alerts for create, update and delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,23 +11,23 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to @post
+      redirect_to @post, success: 'Статья успешно добавлена'
     else
-      render :new
+      render :new, danger: 'Статья не создана'
     end
   end
   def edit
   end
   def update
     if @post.update_attributes(post_params)
-      redirect_to @post
+      redirect_to @post, success: 'Статья успешно обновлена'
     else
-      render :edit
+      render :edit, danger: 'Статья не обновлена'
     end
   end
   def destroy
     @post.destroy
-    redirect_to posts_path
+    redirect_to posts_path, success: 'Статья успешно удалена'
   end
 
   private
