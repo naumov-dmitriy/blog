4cac1f17d3a40063da72235fdf02675978337302
Dmitriy <investacc@yandex.ru>
2018-08-19 17:40:38 +0500

    Add post pagination

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'jquery-rails'
 gem 'carrierwave', '~> 1.0'
 gem 'mini_magick'
 gem 'trix', git: 'https://github.com/bcoia/trix.git'
+gem 'will_paginate', '~> 3.1.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,17 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [ :show, :edit, :update, :destroy ]
+
   def index
-    @posts = Post.all
+    @posts = Post.paginate(:page => params[:page], :per_page => 3)
   end
+
   def show
   end
+
   def new
     @post = Post.new
   end
+
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -16,8 +20,10 @@ class PostsController < ApplicationController
       render :new, danger: 'Статья не создана'
     end
   end
+
   def edit
   end
+
   def update
     if @post.update_attributes(post_params)
       redirect_to @post, success: 'Статья успешно обновлена'
@@ -25,6 +31,7 @@ class PostsController < ApplicationController
       render :edit, danger: 'Статья не обновлена'
     end
   end
+
   def destroy
     @post.destroy
     redirect_to posts_path, success: 'Статья успешно удалена'
@@ -34,6 +41,7 @@ class PostsController < ApplicationController
     def set_post
       @post = Post.find(params[:id])
     end
+
     def post_params
       params.require(:post).permit(:title, :summary, :body, :image)
     end
